aa8c164262227aa05c696fbfdcd7d8b3e8340c63
Lawrence Lee <lawrenceleejr@gmail.com>
2025-07-01 12:30:36 +0200

    fixing font. adding some other helper lines. adding a few more sloppily extracted curves

--- plot.py
+++ plot.py
@@ -1,12 +1,6 @@
 from matplotlib_tufte import *
 setup()
 
-import matplotlib.font_manager as fm
-fm.fontManager.addfont("MyriadPro-Regular.ttf")
-fm.fontManager.addfont("MyriadPro-Bold.ttf")
-from matplotlib import rcParams
-rcParams['font.family'] = 'Myriad Pro'
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -18,12 +12,12 @@ sys.path.insert(0, "..")
 from helperFunctions import *
 
 # plt.subplots_adjust(wspace=0.03)
-def fb_to_hz(cross_section_fb, lumi_cm2_s=1e34):
+def fb_to_hz(cross_section_fb, lumi_cm2_s=2e35):
     # Convert fb to cm^2
     sigma_cm2 = cross_section_fb * 1e-39
     # Rate in Hz
     return sigma_cm2 * lumi_cm2_s
-def hz_to_fb(rate_hz, lumi_cm2_s=1e34):
+def hz_to_fb(rate_hz, lumi_cm2_s=2e35):
     return rate_hz / lumi_cm2_s * 1e39
 
 
@@ -43,7 +37,11 @@ data = {}
 
 data["collisionrate"] = np.genfromtxt("data/collisionrate.txt", delimiter=",", skip_header=0, names=["x","y"])
 
-data["qq"] = np.genfromtxt("data/qq.txt", delimiter=",", skip_header=0, names=["x","y"])
+data["vbfqq"] = np.genfromtxt("data/vbfqq.txt", delimiter=",", skip_header=0, names=["x","y"])
+data["vbftt"] = np.genfromtxt("data/vbftt.txt", delimiter=",", skip_header=0, names=["x","y"])
+data["vbfh"] = np.genfromtxt("data/vbfh.txt", delimiter=",", skip_header=0, names=["x","y"])
+# 
+data["vbfhhh"] = np.genfromtxt("data/vbfhhh.txt", delimiter=",", skip_header=0, names=["x","y"])
 
 
 
@@ -52,17 +50,25 @@ fig, ax = plt.subplots(1,1, figsize=(1.5*baselength, 2*baselength))
 
 # Add manually scaled Y axis on the right
 ax2 = ax.secondary_yaxis('right', functions=(fb_to_hz,hz_to_fb))
-ax2.set_ylabel('Rate [Hz]', color='black')
+ax2.set_ylabel(r'Rate (at L=$2\times10^{35}$ cm$^{-2}$ s$^{-1}$) [Hz]', color='black')
 ax2.tick_params(axis='y', labelcolor='black')
 ax2.set_yscale('log',base=10)
 
-# Put several collision rate curves for different ring assumptions.
-ax.plot(data["collisionrate"]['x'], hz_to_fb(data["collisionrate"]['y']),":", color="black")
-
-ax.text( 0.95*10, 1.05*hz_to_fb(data["collisionrate"]['y'])[-1], 
-    "Collision Rate", 
-    color="black", fontsize=10, verticalalignment='bottom',horizontalalignment='right'
+ax.annotate(
+    'Collision Rate (dummy)',       # Text
+    xy=(10, hz_to_fb(data["collisionrate"]['y'])[-1]),                 # Point to annotate
+    xytext=(8, hz_to_fb(data["collisionrate"]['y'])[-1]),           # Position of the text (to the left)
+    arrowprops=dict(arrowstyle='-|>'), va="center", ha="right"
 )
+
+ax.annotate(
+    '1 Event / Snowmass Year',       # Text
+    xy=(10, hz_to_fb(1e-7)),                 # Point to annotate
+    xytext=(8, hz_to_fb(1e-7)),           # Position of the text (to the left)
+    arrowprops=dict(arrowstyle='-|>'), va="center", ha="right"
+)
+
+
 
 ### Actual Curves:
 
@@ -70,19 +76,48 @@ ax.text( 0.95*10, 1.05*hz_to_fb(data["collisionrate"]['y'])[-1],
 i=0
 alpha=1
 
-ax.plot(data["qq"]['x'], (data["qq"]['y']),"--", color=to_rgba(colors[i],alpha), mew=1)
-
-ax.text( 0.95*10, 1.05*data["qq"]['y'][-1], 
-    r"$q\bar{q}$", 
+ax.plot(data["vbfqq"]['x'], (data["vbfqq"]['y']),"--", color=to_rgba(colors[i],alpha), mew=1)
+ax.text( 0.95*10, 1.05*data["vbfqq"]['y'][-1], 
+    r"VBF $q\bar{q}$", 
     color=to_rgba(colors[i],alpha), fontsize=10, verticalalignment='bottom',horizontalalignment='right'
 )
+
+
+i=i+1
+ax.plot(data["vbftt"]['x'], (data["vbftt"]['y']),"--", color=to_rgba(colors[i],alpha), mew=1)
+ax.text( 0.95*10, 1.05*data["vbftt"]['y'][-1], 
+    r"VBF $t\bar{t}$", 
+    color=to_rgba(colors[i],alpha), fontsize=10, verticalalignment='bottom',horizontalalignment='right'
+)
+
+i=i+1
+ax.plot(data["vbfh"]['x'], (data["vbfh"]['y']),"--", color=to_rgba(colors[i],alpha), mew=1)
+ax.text( 0.95*10, 1.05*data["vbfh"]['y'][-1], 
+    r"VBF $H$", 
+    color=to_rgba(colors[i],alpha), fontsize=10, verticalalignment='bottom',horizontalalignment='right'
+)
+
+i=i+1
+ax.plot(data["vbfhhh"]['x'], (data["vbfhhh"]['y']),"--", color=to_rgba(colors[i],alpha), mew=1)
+ax.text( 0.95*10, 1.05*data["vbfhhh"]['y'][-1], 
+    r"VBF $HHH$", 
+    color=to_rgba(colors[i],alpha), fontsize=10, verticalalignment='bottom',horizontalalignment='right'
+)
+
+
+
+
+
+
+
+
 
 
 ax.set_xlabel(r'$\sqrt{s}$ [TeV]',)
 ax.set_ylabel(r'$\sigma$ [fb]',)
 ax.set_xscale('log',base=10)
 ax.set_yscale('log',base=10)
-ax.set_ylim([1e2,2e10])
+ax.set_ylim([1e-4,2e9])
 ax.set_xlim([1,10])
 
 
