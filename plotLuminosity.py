940a0b14a5bfa4567a28967d4baad2287b36bfdf
Tova Holmes <tova.r.holmes@gmail.com>
2025-07-25 11:41:04 +0200

    Updated lumi plot style.

--- plotLuminosity.py
+++ plotLuminosity.py
@@ -26,29 +26,59 @@ for i in range(len(t)):
     L[i] = L0 * np.exp(-2 * t_since_refill / tau_mu)
     BIB[i] = np.exp(-t_since_refill / tau_mu)  # normalized to 1.0 at t=0
 
+'''
+# === Plotting ===
+fig, (ax1, ax2) = plt.subplots(
+    2, 1, figsize=(10, 6), sharex=True, gridspec_kw={"hspace": 0.25}
+)
+
+# -- Top: Luminosity --
+ax1.plot(t, L / 1e34, color='tab:blue', label="$L(t)$")
+ax1.axhline(L_avg / 1e34, linestyle='--', color='gray', label="Average $L$")
+ax1.set_ylabel(r"Inst. Luminosity [$10^{34}$ cm$^{-2}$ s$^{-1}$]")
+ax1.legend(loc="upper right")
+#ax1.grid(True)
+
+# -- Bottom: BIB --
+ax2.plot(t, BIB, color='tab:red', label="Beam-Induced Background")
+ax2.set_xlabel("Time [s]")
+ax2.set_ylabel(r"Fraction of Peak BIB")
+ax2.set_ylim(0, 1.05)
+#ax2.legend(loc="upper right")
+#ax2.grid(True)
+
+# -- Final layout --
+#fig.suptitle("Instantaneous Luminosity and Beam-Induced Background Over Time", fontsize=13)
+plt.tight_layout(rect=[0, 0, 1, 0.96])
+#plt.show()
+fig.savefig("InstantaneousLuminosity.pdf")
+'''
+
 # Plot
 fig, ax1 = plt.subplots(figsize=(10, 5))
 
 # Left axis: normalized luminosity
-ax1.plot(t, L / 1e34, label=r'$L(t)$', color='tab:blue')
-ax1.axhline(L_avg / 1e34, color='gray', linestyle='--', label='Average $L$')
+ax1.plot(t, L / L0, label=r'$L(t)/L(t=0)$', color='tab:blue')
+ax1.axhline(L_avg / L0, color='gray', linestyle='--', label='Average $L$')
+ax1.plot(t, BIB, label='BIB$(t)$/BIB$(t=0)$', color='tab:red')
 ax1.set_xlabel("Time [s]")
-ax1.set_ylabel(r"Instantaneous Luminosity [$10^{34}$ cm$^{-2}$ s$^{-1}$]", color='tab:blue')
-ax1.tick_params(axis='y', labelcolor='tab:blue')
-ax1.set_ylim([0, 5])
+ax1.set_ylabel("Fraction of Peak Value")
+#ax1.set_ylabel(r"Instantaneous Luminosity [$10^{34}$ cm$^{-2}$ s$^{-1}$]", color='tab:blue')
+ax1.tick_params(axis='y')#, labelcolor='tab:blue')
+ax1.set_ylim([0, 1.3])
 #ax1.grid(True)
 
 # Right axis: relative BIB
-ax2 = ax1.twinx()
-ax2.plot(t, BIB, label='Beam-Induced Background', color='tab:red')
-ax2.set_ylabel("Fraction of Peak Beam-Induced Background", color='tab:red')
-ax2.tick_params(axis='y', labelcolor='tab:red')
-ax2.set_ylim(0, 1.5)
+#ax2 = ax1.twinx()
+#ax2.plot(t, BIB, label='Beam-Induced Background', color='tab:red')
+#ax2.set_ylabel("Fraction of Peak Beam-Induced Background", color='tab:red')
+#ax2.tick_params(axis='y', labelcolor='tab:red')
+#ax2.set_ylim(0, 1.5)
 
 # Legends
 lines1, labels1 = ax1.get_legend_handles_labels()
-lines2, labels2 = ax2.get_legend_handles_labels()
-ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper right')
+#lines2, labels2 = ax2.get_legend_handles_labels()
+ax1.legend(lines1, labels1, loc='upper right')
 
 #plt.title("Instantaneous Luminosity and Beam-Induced Background Over Time")
 plt.tight_layout()
